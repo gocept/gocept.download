a0990714c22e0357d0e089ca039c79890b2ae00c
Thomas Lotze <tl@gocept.com>
2008-06-06 08:22:41 +0000

    added more classifiers and a test extra, improved long description

--- README.txt
+++ README.txt
@@ -4,3 +4,20 @@ gocept.download
 
 
 zc.buildout recipe to download and extract an archive.
+
+
+Configuration options
+=====================
+
+:url:
+    The URL of the archive to download.
+
+:strip-top-level-dir: (optional)
+    Whether to remove the top-level directory from the extracted archive
+    contents.
+
+:md5sum:
+    MD5 checksum of the archive.
+
+:destination:
+    Where to put the extracted archive contents.
--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 
+
 name = "gocept.download"
+
+classifiers = [
+    "Environment :: Console",
+    "Environment :: Plugins",
+    "Framework :: Buildout",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Zope Public License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Build Tools",
+    "Topic :: System :: Software Distribution",
+    ]
+
 setup(
     name = name,
     version = "dev",
@@ -10,8 +25,8 @@ setup(
     "zc.buildout recipe for downloading and extracting an archive.",
     long_description = open("README.txt").read(),
     license = "ZPL 2.1",
-    keywords = "buildout",
-    classifiers = ["Framework :: Buildout"],
+    keywords = "buildout zc.buildout recipe download extract archive",
+    classifiers = classifiers,
     url = "http://svn.gocept.com/repos/gocept/" + name,
     download_url = \
     "https://svn.gocept.com/repos/gocept/"
@@ -21,7 +36,6 @@ setup(
     package_dir = {"": "src"},
     namespace_packages = ["gocept"],
     install_requires = ["zc.buildout", "setuptools"],
-    entry_points = {"zc.buildout": ["default = %s:Recipe" % name,
-                                    ],
-                    },
+    extras_require = {"test": ["zope.testing"]},
+    entry_points = {"zc.buildout": ["default = %s:Recipe" % name,],},
     )
