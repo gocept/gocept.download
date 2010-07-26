1144113c7428a88b4877e3c3466ab876fa3f72d1
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2010-07-26 08:36:08 +0000

    updated copyright header

--- src/gocept/download/__init__.py
+++ src/gocept/download/__init__.py
@@ -1,16 +1,5 @@
-##############################################################################
-#
-# Copyright (c) 2007 gocept gmbh & co. kg and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
+# Copyright (c) 2007-2010 gocept gmbh & co. kg
+# See also LICENSE.txt
 
 import os
 import os.path
